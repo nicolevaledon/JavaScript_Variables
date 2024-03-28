d463e8453ddfaa2fd7d7c1c0a2556d89fdc01593
Nicole Valedon <nicole.valedon@gmail.com>
2024-03-28 14:59:27 -0400

    JS Variables Exercise

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
-
+const firstName = "Nicole";
+lastName = "Valedon";
+age = 29;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salepercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
